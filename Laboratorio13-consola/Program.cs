c436b96d92dd45292a2103a95eb4ec27c91b8992
CRISTHIAN <CRISTHIAN@DESKTOP-LS2ABPT>
2023-11-14 22:31:25 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio13-consola/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio13_consola
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = Pantallas.pantallaPrincipal();
+            do
+            {
+                switch (opcion)
+                {
+                    case 0:
+                        opcion = Pantallas.pantallaPrincipal();
+                        break;
+                    case 1:
+                        opcion = Pantallas.encuesta();
+                        break;
+                    case 2:
+                        opcion = Pantallas.ordenarDatos();
+                        break;
+                    case 3:
+                        opcion = Pantallas.elimininarDatos();
+                        break;
+                    
+                }
+            } while (opcion != 4);
+
+        }
+    }
+}
+
